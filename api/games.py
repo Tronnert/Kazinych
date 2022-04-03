46a6d95e15533d48762d404a02f7c2bc09af9d5e
arseniy.kolomeytsev@gmail.com <arseniy.kolomeytsev@gmail.com>
2022-04-03 15:07:20 +0300

    супер_пупер_кибер_рулетка

--- api/games.py
+++ api/games.py
@@ -17,11 +17,22 @@ class Roulette(Game):
     def get_win(self, json):
         win = 0
         ch = random.randint(1, 37)
-        if ch == json['number']:
-            win = json['bet']
+        if ch == int(json['number']):
+            win = int(json['bet'])
         return win, ch
 
-games_dict = {'roulette': Roulette()}
+class Cyber_Roulette:
+    def __init__(self):
+        pass
+
+    def get_win(self, json):
+        win = 0
+        ch = random.randint(1, 8)
+        if ch == int(json['number']):
+            win = int(json['bet'])
+        return win, ch
+
+games_dict = {'roulette': Roulette(), 'cyber_roulette': Cyber_Roulette()}
 # first_roulette = Game('first', 0.2, 0.5, 1, 1.2, 1.5)
 # games_dict.update(first_roulette.get())
 
