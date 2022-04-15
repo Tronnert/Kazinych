8f4508843d072af46319e51e7b9ea6830ff6802e
arseniy.kolomeytsev@gmail.com <arseniy.kolomeytsev@gmail.com>
2022-04-16 00:24:40 +0300

    mime not working

--- api/spammer.py
+++ api/spammer.py
@@ -1,17 +1,18 @@
-from threading import Thread
 import schedule
 import smtplib
 from data import db_session
-import sqlalchemy
 from flask import render_template
 import time
 from datetime import datetime
 from data.users import User
 from data.balance_changes import BalanceChanges
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from flask_login import current_user
 
 class Spammer:
     def __init__(self):
-        schedule.every(24).hours.do(self.send_emails)
+        schedule.every(15).seconds.do(self.send_emails)
         while True:
             schedule.run_pending()
             time.sleep(1)
@@ -19,14 +20,19 @@ class Spammer:
 
     def send_emails(self):
         print(1)
+        global app
         self.server = smtplib.SMTP_SSL('smtp.mail.ru', 465)
         self.address = "spammer.noreply@mail.ru"
         self.server.login(self.address, 'hkV2AH1txBPhFh2D7nZa')
+        db_sess = db_session.create_session()
         for user in db_session.create_session().query(User).filter(User.email_flag):
-            if (datetime.now() - \
-                    db_session.create_session().query(BalanceChanges).filter(BalanceChanges.user_id ==
-                                                                                         user.id).order_by(BalanceChanges.date.desc()).first().date).total_seconds() > 172800:
-                self.server.sendmail(self.address, user.email, str((datetime.now() - \
-                    db_session.create_session().query(BalanceChanges).filter(BalanceChanges.user_id ==
-                                                                                         user.id).order_by(BalanceChanges.date.desc()).first().date).total_seconds()))
+            if (datetime.now() - db_sess.query(BalanceChanges).filter(BalanceChanges.user_id == user.id).order_by(BalanceChanges.date.desc()).first().date).total_seconds() > 0:
+                dt = (datetime.now() - db_sess.query(BalanceChanges).filter(BalanceChanges.user_id == user.id).order_by(BalanceChanges.date.desc()).first().date).total_seconds()
+                msg = MIMEMultipart("alternative")
+                msg["Subject"] = "multipart test"
+                msg["From"] = 'Спаммер Спаммерович'
+                with app.app_context():
+                    html_msg = MIMEText(render_template('templates/email.html', name=user.name, time=dt), "html")
+                self.server.sendmail(self.address, user.email, str(dt))
+                print(2)
         self.server.quit()
